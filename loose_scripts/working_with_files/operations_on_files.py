4f33399fb5fba7d5496206c6938987904293a184
Maciej Kaliś <mackal1993@gmail.com>
2023-03-17 20:04:04 +0100

    Adding operations_on_files and sample .txt file

---
+++ loose_scripts/working_with_files/operations_on_files.py
@@ -0,0 +1,28 @@
+"""
+Load names and surnames from a file named:
+namessurnames.txt
+
+1) split them up to make a list of tuples, where the inner tuples are
+first/last pairs
+2) save the names to the file names.txt
+3) save the names to the file surnames.txt
+
+"""
+
+namesAndSurnames = []
+
+with open("namessurnames.txt", "r", encoding="UTF-8") as file:
+    for line in file:
+        namesAndSurnames.append(tuple(line.replace("\n", "").split(" ")))
+
+
+with open("names.txt", "w", encoding="UTF-8") as file:
+    for item in namesAndSurnames:
+        file.write(item[0] + "\n")
+
+with open("surnames.txt", "w", encoding="UTF-8") as file:
+    for item in namesAndSurnames:
+        try:
+            file.write(item[1] + "\n")
+        except IndexError:
+            file.write("\n")
